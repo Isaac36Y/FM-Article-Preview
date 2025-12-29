ab28dabbfc28e4fbba655b93fff4755e6a1a4983
Isaac Young <isaac3.young6@gmail.com>
2025-12-28 17:05:29 -0800

    got the share buttons working. Lots of kinks to iron out

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const shareBtn = document.getElementById("share-button");
 const removeShareBtn = document.getElementById("sharing-button");
 const feature = document.getElementById("feature");
 const sharing = document.getElementById("sharing")
+const popUp = document.getElementById("pop-up")
+const screenWidth = window.innerWidth
 
 shareBtn.addEventListener("click", () => {
-    feature.classList.add('hidden');
-    sharing.classList.remove('hidden');
+    if (screenWidth <= 768) {
+        feature.classList.add('hidden');
+        sharing.classList.remove('hidden');  
+    }else {
+        popUp.classList.toggle('hidden');
+    }
+    
 })
 
 removeShareBtn.addEventListener("click", () => {
-    feature.classList.remove('hidden');
-    sharing.classList.add('hidden');
+        feature.classList.remove('hidden');
+        sharing.classList.add('hidden');  
+   
 })
